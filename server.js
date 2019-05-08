f12eb4cd0ab6d7c0acd74621f7f46a0965d2b332
Allegra Anka <allegra.anka@gmail.com>
2019-05-07 23:34:31 -0400

    server

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 var express = require("express");
 var path = require("path");
 
-<<<<<<< HEAD
 var port = process.env.PORT || 3100;
-=======
-var port = 5111;
->>>>>>> 4b540385174ba92ce74c5f839dcd8a62d7e2ccdc
 var app = express();
 
 app.use(express.urlencoded({ extended: true }));
